f4b14e3d93fc47dccc8a1719fd3456987dae6f93
Ravi <ravteja.mullapudi@gmail.com>
2015-12-18 00:29:10 -0500

    Plotting Speedups

--- apps/benchmark.py
+++ apps/benchmark.py
@@ -1,10 +1,28 @@
 import matplotlib.pyplot as plt
-times = {}
-for mode in ["ref_perf.txt", "auto_perf.txt"]:
-    for app in ["blur/", "local_laplacian/", "interpolate/", "bilateral_grid/", \
-                "camera_pipe/"] :
+times_ref = {}
+times_auto = {}
+speed_up = {}
+p = 1
+for app in ["blur/", "local_laplacian/", "interpolate/", "bilateral_grid/", \
+            "camera_pipe/"] :
+        f = open(app+ "ref_perf.txt")
+        times_ref[app] = [ float(l) for l in f ]
+        f.close()
+        f = open(app+ "auto_perf.txt")
+        times_auto[app] = [ float(l) for l in f ]
+        f.close()
+        speed_up[app] = []
+        for i in xrange(0, len(times_auto[app])):
+            speed_up[app].append(times_ref[app][i]/times_auto[app][i])
 
-        f = open(app+mode)
-        times[app] = [ float(l) for l in f ]
-        print times[app]
-    print
+        ax = plt.subplot(2, 3, p)
+        ax.bar([1, 2, 3, 4], speed_up[app], alpha=0.4, align='center')
+        ax.set_xlabel("Threads")
+        ax.set_ylabel("Speed Up")
+        ax.set_ylim([0, 1])
+        ax.set_xticks([1, 2, 3 , 4])
+        ax.set_xticklabels(["1", "2", "4", "8"])
+        ax.set_title(app)
+        p = p+1
+        print speed_up[app]
+plt.show()
