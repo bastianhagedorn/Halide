b8468fb86248728f68f933c2181c03b05f68eb8e
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2015-12-31 22:12:14 -0800

    benchmark.py cleanup

--- apps/benchmark.py
+++ apps/benchmark.py
@@ -1,31 +1,46 @@
+# from ggplot import *
+# import pandas as pd
+
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
+import os.path
+import math
+
 times_ref = {}
 times_auto = {}
 speed_up = {}
 p = 1
-for app in ["blur/", "unsharp/", "harris/", "local_laplacian/", "interpolate/", "bilateral_grid/", \
-            "camera_pipe/", "conv_layer/", "cost_function_test/", "overlap_test/", \
-            "split_test/", "tile_vs_inline_test/", "data_dependent_test/",\
-            "parallel_test/"] :
-        f = open(app+ "ref_perf.txt")
-        times_ref[app] = [ float(l) for l in f ]
-        f.close()
-        f = open(app+ "auto_perf.txt")
-        times_auto[app] = [ float(l) for l in f ]
-        f.close()
-        speed_up[app] = []
-        for i in xrange(0, len(times_auto[app])):
-            speed_up[app].append(times_ref[app][i]/times_auto[app][i])
+
+for app in ["blur", "unsharp", "harris", "local_laplacian", "interpolate", "bilateral_grid", \
+            "camera_pipe", "conv_layer", "cost_function_test", "overlap_test", \
+            "split_test", "tile_vs_inline_test", "data_dependent_test",\
+            "parallel_test"] :
+
+        with open(os.path.join(app, "ref_perf.txt")) as f:
+            times_ref[app] = [ float(l) for l in f ]
+
+        with open(os.path.join(app, "auto_perf.txt")) as f:
+            times_auto[app] = [ float(l) for l in f ]
+
+        speed_up[app] = [ref/auto for (ref,auto) in zip(times_ref[app], times_auto[app])]
 
         ax = plt.subplot(4, 4, p)
         ax.bar([1, 2, 3, 4], speed_up[app], alpha=0.4, align='center')
         ax.set_xlabel("Threads")
         ax.set_ylabel("Speed Up")
-        ax.set_ylim([0, 1])
+        ax.set_ylim([0, math.ceil(max(speed_up[app]))])
         ax.set_xticks([1, 2, 3 , 4])
         ax.set_xticklabels(["1", "2", "4", "8"])
-        ax.set_title(app)
+        ax.set_title(app.replace('_',' ').title())
         p = p+1
         print speed_up[app]
-plt.tight_layout()
-plt.show()
+
+fig = plt.gcf()
+fig.set_size_inches(20,20)
+plt.tight_layout(pad=3.0)
+
+# plt.show()
+plt.savefig('benchmarks.png', dpi=150)
