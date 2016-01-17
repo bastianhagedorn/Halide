304503b297005e958f91af208a95af08ebbce364
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2016-01-17 05:58:45 -0800

    Improved schmoo.sh
    - use clean new `make auto` target
    - parameterize num threads

--- apps/schmoo.sh
+++ apps/schmoo.sh
@@ -4,7 +4,7 @@ apps=`cat apps.txt`
 
 halide_dir=${HOME}/ravi
 
-start_time=`date "+%Y-%m-%d.%H%M"`
+start_time=`date "+%Y-%m-%d.%H%M%S"`
 rundirname="${HOSTNAME}-schmoo-${start_time}"
 mkdir -p $rundirname
 rundir=`realpath $rundirname`
@@ -26,6 +26,7 @@ MEMSIZE=${MEMSIZE:-131072 524288 2097152 8388608 33554432}
 BALANCE=${BALANCE:-1 2 3 5 7 10 14 19 28}
 VEC=${VEC:-4 8}
 PAR=${PAR:-1 2 4 8}
+THREADS=${THREADS:-1 2 4 8}
 
 #echo "This will remove {gen,run}.*.txt - OK?"; read
 for app in $apps; do
@@ -69,15 +70,17 @@ for app in $apps; do
                     export HL_AUTO_VEC_LEN=$vec
                     export HL_AUTO_BALANCE=$balance
                     export HL_AUTO_FAST_MEM_SIZE=$memsize
-                    if [[ -f ./regen.sh ]]; then
-                        bash ./regen.sh 2>> $errlog > $genfile
-                    else
-                        rm -f "*.o"
-                        make -s cleangen 2>> $errlog
-                        make -s gen > $genfile 2>> $errlog
-                        #echo "$WARN<--!$ must cull out naive gen log data!!" >> $genfile # should be cool now
-                    fi
-                    for runthreads in 1 2 4 8
+                    # if [[ -f ./regen.sh ]]; then
+                    #     bash ./regen.sh 2>> $errlog > $genfile
+                    # else
+                        # rm -f "*.o"
+                        # make -s cleangen 2>> $errlog
+                        # make -s gen > $genfile 2>> $errlog
+                        # echo "$WARN<--!$ must cull out naive gen log data!!" >> $genfile # Gen data gets shoved in twice - need to remove duplicate keys
+                    # fi
+                    make -s clean_auto 2>> $errlog
+                    make -s auto > $genfile 2>> $errlog
+                    for runthreads in $THREADS
                     do
                         echo "          numthreads: $runthreads"
                         runfile="$apprundir/run.$par.$vec.$balance.$memsize.$runthreads.txt"
