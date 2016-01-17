159abecffc4740a4dd19185097ccdc7b80e7694f
Andrew Adams <andrew.b.adams@gmail.com>
2016-01-16 18:03:45 -0800

    Make max_filter work with autoscheduler

--- apps/max_filter/max_filter.cpp
+++ apps/max_filter/max_filter.cpp
@@ -21,22 +21,23 @@ int main(int argc, char **argv) {
     }
     int schedule = atoi(argv[3]);
 
+    Image<float> in = load_image(argv[1]);
+
     ImageParam input_im(Float(32), 3);
-    Param<int> radius;
+    const int radius = 26;
 
     Func input = BoundaryConditions::repeat_edge(input_im);
 
     Var x, y, c, t;
 
-    Expr slices = cast<int>(ceil(log(radius) / logf(2))) + 1;
+    const int slices = (int)(ceilf(logf(radius) / logf(2))) + 1;
 
     // A sequence of vertically-max-filtered versions of the input,
     // each filtered twice as tall as the previous slice. All filters
     // are downward-looking.
     Func vert_log;
-    vert_log(x, y, c, t) = undef<float>();
-    vert_log(x, y, c, 0) = input(x, y, c);
-    RDom r(-radius, input_im.height() + radius, 1, slices-1);
+    vert_log(x, y, c, t) = input(x, y, c);
+    RDom r(-radius, in.height() + radius, 1, slices-1);
     vert_log(x, r.x, c, r.y) = max(vert_log(x, r.x, c, r.y - 1), vert_log(x, r.x + clamp((1<<(r.y-1)), 0, radius*2), c, r.y - 1));
 
     // We're going to take a max filter of arbitrary diameter
@@ -76,11 +77,11 @@ int main(int argc, char **argv) {
 
         final.compute_root().split(x, tx, x, 256).reorder(x, y, c, tx).fuse(c, tx, t).parallel(t).vectorize(x, 8);
         vert_log.compute_at(final, t);
-        vert_log.update(0).vectorize(x, 8);
-        vert_log.update(1).reorder(x, r.x, r.y, c).vectorize(x, 8);
+        vert_log.vectorize(x, 8);
+        vert_log.update().reorder(x, r.x, r.y, c).vectorize(x, 8);
         vert.compute_at(final, y).vectorize(x, 8);
     break;
-    
+
     default:
         vert_log.compute_root();
         vert.compute_root();
@@ -93,9 +94,8 @@ int main(int argc, char **argv) {
 
     // Run it
 
-    Image<float> in = load_image(argv[1]);
     input_im.set(in);
-    radius.set(26);
+    //radius.set(26);
     Image<float> out(in.width(), in.height(), in.channels());
     Target target = get_target_from_environment();
     if (schedule == -1) {
