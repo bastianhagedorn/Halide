c26f7ec13efb393e3e71d086b7d52bd6dffe7401
Jonathan Ragan-Kelley <jrk@cs.stanford.edu>
2016-01-17 20:01:52 -0800

    Make benchmark.sh log intio dedicated benchmark run dir

--- apps/benchmark.sh
+++ apps/benchmark.sh
@@ -3,6 +3,22 @@ APPS=`cat apps.txt`
 RACES=`cat raceapps.txt`
 TESTS=`cat tests.txt`
 CONV=`cat conv.txt`
+
+myrealpath () {
+  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+start_time=`date "+%Y-%m-%d.%H%M%S"`
+rundirname="bench-${HOSTNAME}-${start_time}"
+mkdir -p $rundirname
+rundir=`myrealpath $rundirname`
+
+halide_hash=`openssl md5 ../bin/libHalide.a  | sed 's/^.* //'`
+
+echo "hostname: ${HOSTNAME}" >> $rundir/config.txt
+echo "start_time: ${start_time}" >> $rundir/config.txt
+echo "libHalide.a: ${halide_hash}" >> $rundir/config.txt
+
 BATCH=""
 while [[ $# > 0 ]]
 do
@@ -43,8 +59,11 @@ for app in $BATCH; do
     echo "============================================================"
     echo "                  BENCHMARKING $app"
     echo "============================================================"
+    # TODO: how to log gen data - autoscheduler?
     make clean; make bench;
-    # TODO: how to log gen data?
+    echo ${rundir}/${app}
+    mkdir -p "${rundir}/${app}"
+    mv *_perf.txt "${rundir}/${app}"
     cd ../;
 done
 #python benchmark.py
