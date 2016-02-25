25a688412fc8807adf40006ded4cc436d2055e4b
Ravi Teja Mullapudi <rmullapu@compute-0-31.local>
2016-02-25 15:54:46 -0500

    Adding in all the sweep parameters on latedays

--- apps/interpolate/test.sh
+++ apps/interpolate/test.sh
@@ -5,7 +5,7 @@ else
     if [[ $1 == "naive" ]]; then
         export HL_AUTO_NAIVE=1
     elif [[ $1 == "sweep" ]]; then
-        export HL_AUTO_SWEEP=1
+        export HL_AUTO_SWEEP=1 HL_AUTO_PARALLELISM=6 HL_AUTO_VEC_LEN=16 HL_AUTO_BALANCE=5 HL_AUTO_FAST_MEM_SIZE=131072
     fi
     sched=-1
 fi
