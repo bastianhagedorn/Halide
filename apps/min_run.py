25102be94c31dfbc087b68a814b122de16a9a9b9
Ravi Teja Mullapudi <rmullapu@compute-0-31.local>
2016-02-25 01:43:08 -0500

    Adding sweep parameters

--- apps/min_run.py
+++ apps/min_run.py
@@ -3,14 +3,13 @@ import glob
 import errno
 path = 'run*.txt'
 files = glob.glob(path)
-min_time = float("inf")
-min_config = ''
+times = []
 for file in files:
     with open(file, 'r') as f:
         for line in f:
             pass
         words = [word.strip() for word in line.split()]
-        if float(words[-1]) < min_time:
-            min_time = float(words[-1])
-            min_config = file
-print min_config, min_time
+        times.append((file, float(words[-1])))
+times.sort(key = lambda x: x[1])
+for t in times:
+    print t
