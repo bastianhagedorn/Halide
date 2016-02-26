5196829e87f2f78e3b9cea7883ed1a009d768bbc
Ravi Teja Mullapudi <rmullapu@compute-0-31.local>
2016-02-26 15:34:52 -0500

    Adding the best schedule variant to benchmarking

--- apps/max_filter/test.sh
+++ apps/max_filter/test.sh
@@ -6,6 +6,8 @@ else
         export HL_AUTO_NAIVE=1
     elif [[ $1 == "sweep" ]]; then
         export HL_AUTO_SWEEP=1 HL_AUTO_PARALLELISM=12 HL_AUTO_VEC_LEN=16 HL_AUTO_BALANCE=1 HL_AUTO_FAST_MEM_SIZE=131072
+    elif [[ $1 == "rand" ]]; then
+        export HL_AUTO_RAND=1 HL_AUTO_PARALLELISM=12 HL_AUTO_VEC_LEN=16 HL_AUTO_BALANCE=1 HL_AUTO_FAST_MEM_SIZE=131072
     fi
     sched=-1
 fi
